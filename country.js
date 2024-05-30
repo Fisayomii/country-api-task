7c3ed0623302911dd62ec81bd5bd6894d76f5fa4
Fisayomii <estherjoshua2002@gmail.com>
2024-05-30 21:54:33 +0100

    9:54

---
+++ country.js
@@ -0,0 +1,54 @@
+const countryDetails = document.getElementById("details-container")
+const countryCode = window.location.search.split("=")[1];
+console.log(countryCode)
+
+async function fetchCountryDetails() {
+    try {
+        if (!countryCode) {
+            alert("Country code not found! Returning to home screen")
+            window.location.href = '/'
+        } else {
+    const response = await fetch('https://restcountries.com/v3.1/all')
+    const countriesData = await response.json()
+    const country = countriesData.find(el => el.cca3 == countryCode)
+    console.log(country)
+    const result = {
+            name: country.name.common,
+            population: country.population.toLocaleString(),
+            continent: country.continents,
+            capital: country.capital,
+            flag: country.flags.png,
+            nativeName: Object.entries(country.name.nativeName)[0][1].common,
+        }
+    
+    // result.forEach(country => {
+    //     // console.log(country.name);
+    // });
+    console.log(result)
+    displayDetails(result)}
+} catch(error) {
+    console.error('There has been a problem with your fetch operation:', error);
+}
+
+}
+
+
+function displayDetails(obj) {
+    let countrydetails =''
+        countrydetails += `
+        <div class="flex">
+        <div>
+                <img src="${obj.flag}" alt="Flag of ${obj.name}" class = "countryimage">
+                </div>
+                <div>
+                <h2>Country: ${obj.name}</h2>
+                <p>Population: ${obj.population}<p>
+                <p>Continent: ${obj.continent}<p>
+                <p>Capital: ${obj.capital}<p>
+                <p>Capital: ${obj.nativeName}<p>
+                <div>
+        </div>`
+    countryDetails.innerHTML = countrydetails
+}
+
+fetchCountryDetails()
---
+++ index.js
@@ -0,0 +1,53 @@
+const showCountries = document.getElementById("card-container");
+const button = document.getElementById("click");
+
+const handleClick = () => {
+  fetchCountries();
+  console.log("button");
+};
+button.addEventListener("click", handleClick);
+let allCountries = [];
+async function fetchCountries() {
+  try {
+    const response = await fetch("https://restcountries.com/v3.1/all");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const countriesData = await response.json();
+    // console.log("test",countriesData[0]);
+    const result = countriesData.map((item) => {
+    //   console.log(item.cca3);
+      return {
+        name: item.name.common,
+        population: item.population.toLocaleString(),
+        continent: item.continents,
+        capital: item.capital,
+        flag: item.flags.png,
+        code: item.cca3
+      };
+    });
+    
+
+   showCountries.innerHTML = result.map((country) => `
+   <a href="country.html?country=${country.code} ">
+        <div class="card">
+        <div class="imgdiv">
+           <img src="${country.flag}" alt="Flag of ${country.name}" class="countryimage" />
+           </div>                
+                    <div class="details">
+                    <h2 class="p-details">${country.name}</h2>
+                    <p class="p-details">Population: ${country.population}<p>
+                    <p class="p-details">Continent: ${country.continent}<p>
+                    <p class="p-details">Capital: ${country.capital}<p>                
+            </div>
+            </div>
+            </a>`)
+    displayCountries(result);
+  } catch (error) {
+    console.error("There has been a problem with your fetch operation:", error);
+  }
+}
+
+
+fetchCountries();
+
